4e913ad3fc8b53826427231a0d5a3d1af2cb973f
Tryferos <truf022@gmail.com>
2023-12-28 16:02:47 +0200

    1st working version of the dropdown component

--- src/components/Dropdown/index.ts
+++ src/components/Dropdown/index.ts
@@ -1,4 +1,3 @@
 import '../../tailwind.css'
 
 export * from './Dropdown.tsx'
-export * from './Search.tsx'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'sm': '25%',
+        'md': '50%',
+        'lg': '75%',
+        'full': '100%',
+      },
+      minHeight: {
+        md: '1rem',
+      },
+      boxShadow: {
+        'outline': '3px 3px 6px 2px #999999',
+        'outline-down': '0px 4px 6px 1px #888888',
+      }
+    },
   },
   plugins: [],
 }
